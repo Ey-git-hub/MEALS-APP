3785e7a028b862ae83a2f58f801552abbbd06a2b
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-06-07 16:08:24 +0300

    saving progress on caterories screen

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,7 +1,11 @@
 import "package:flutter/material.dart";
 
 class Category {
-  Category(this.id,this.title)
+  Category({
+    required this.id,
+    required this.title,
+    this.color = Colors.orangeAccent,
+  });
   final String id;
   final String title;
   final Color color;
