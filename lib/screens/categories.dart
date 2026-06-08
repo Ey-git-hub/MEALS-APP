19e0e9831b232122877939c9adbc94df54d93516
Ey-git-hub <eyob.tadesse@astu.edu.et>
2026-06-08 13:53:23 +0300

    adding_data_to_target_screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,14 +1,22 @@
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+// import 'package:meals_app/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'some title', meals: []),
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals),
       ),
     ); //Navigator.push(context,route)
   }
@@ -31,7 +39,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectcategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
